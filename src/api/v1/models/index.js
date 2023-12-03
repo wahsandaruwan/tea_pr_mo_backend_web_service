b099d1748244553cbee94a3d33845dd23423d763
Himal Sandaruwan <wahsandaruwan6@gmail.com>
2023-12-03 23:18:39 +0530

    Created a import and export

---
+++ src/api/v1/models/index.js
@@ -0,0 +1,17 @@
+// ----------Imports----------
+const UserModel = require("./User");
+const MeasurementModel = require("./Measurement");
+const NotificationModel = require("./Notification");
+const DeviceModel = require("./Device");
+const RuleModel = require("./Rule");
+const StepModel = require("./Step");
+
+// ----------Exports----------
+module.exports = {
+  UserModel,
+  MeasurementModel,
+  NotificationModel,
+  DeviceModel,
+  RuleModel,
+  StepModel,
+};
